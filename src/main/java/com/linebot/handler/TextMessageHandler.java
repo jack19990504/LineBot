2e9c4d4e85f1a45e2ed9b1102f577ef1c002cd4c
jack19990504 <jack19990504@gmail.com>
2021-03-25 22:40:49 +0800

    feat : add handlers

---
+++ src/main/java/com/linebot/handler/TextMessageHandler.java
@@ -0,0 +1,21 @@
+package com.linebot.handler;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@LineMessageHandler
+@Slf4j
+public class TextMessageHandler {
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        log.info("event: {}",event);
+        return new TextMessage(event.getMessage().getText());
+    }
+
+}
